591bcaad9a119e729f263e82ed4668cb8c3f1cf3
Leonardo Blautzik <leoblautzik@gmail.com>
2024-06-14 17:54:27 -0300

    gruvbox, dracula, tokyonight themes agregados

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		-- priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	name = "dracula",
+	-- 	-- priority = 1000,
+	-- 	config = function()
+	-- 		--	vim.cmd.colorscheme("dracula")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"sainnhe/gruvbox-material",
+	-- 	name = "gruvbox",
+	-- 	-- priority = 1000,
+	-- 	config = function()
+	-- 		--	vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("tokyonight-night")
+	-- 	end,
+	-- },
+}
