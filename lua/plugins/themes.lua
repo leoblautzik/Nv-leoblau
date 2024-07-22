02d5ae173366878caafed7a04c924754e3ec4b6f
Leonardo Blautzik <leoblautzik@gmail.com>
2024-07-22 11:30:53 -0300

    Se agregan linters y formatting para python

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -7,27 +7,27 @@ return {
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
-	-- {
-	-- 	"Mofiqul/dracula.nvim",
-	-- 	name = "dracula",
-	-- 	-- priority = 1000,
-	-- 	config = function()
-	-- 		--	vim.cmd.colorscheme("dracula")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"sainnhe/gruvbox-material",
-	-- 	name = "gruvbox",
-	-- 	-- priority = 1000,
-	-- 	config = function()
-	-- 		--	vim.cmd.colorscheme("gruvbox")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("tokyonight-night")
-	-- 	end,
-	-- },
+	{
+		"Mofiqul/dracula.nvim",
+		name = "dracula",
+		-- priority = 1000,
+		config = function()
+			--	vim.cmd.colorscheme("dracula")
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		name = "gruvbox",
+		-- priority = 1000,
+		config = function()
+			--	vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
 }
