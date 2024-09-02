d16250a842e22d9761e52153f0d5f38d92137294
Leonardo Blautzik <leoblautzik@gmail.com>
2024-09-01 21:32:01 -0300

    Compilar y ejecutar .c y .py con space ex

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -11,27 +11,27 @@ return {
 			-- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none", ctermbg = "none" })
 		end,
 	},
-	{
-		"Mofiqul/dracula.nvim",
-		name = "dracula",
-		-- priority = 1000,
-		config = function()
-			--	vim.cmd.colorscheme("dracula")
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		name = "gruvbox",
-		-- priority = 1000,
-		config = function()
-			--	vim.cmd.colorscheme("gruvbox")
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		-- priority = 1000,
-		config = function()
-			-- vim.cmd.colorscheme("tokyonight-night")
-		end,
-	},
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	name = "dracula",
+	-- 	-- priority = 1000,
+	-- 	config = function()
+	-- 		--	vim.cmd.colorscheme("dracula")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"sainnhe/gruvbox-material",
+	-- 	name = "gruvbox",
+	-- 	-- priority = 1000,
+	-- 	config = function()
+	-- 		--	vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	-- priority = 1000,
+	-- 	config = function()
+	-- 		-- vim.cmd.colorscheme("tokyonight-night")
+	-- 	end,
+	-- },
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -24,18 +24,32 @@ vim.api.nvim_set_keymap("n", "<leader>sx", "<cmd>close<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Tab>", "<cmd>bn<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<S-Tab>", "<cmd>bp<CR>", { noremap = true })
 
+-- compilar y ejecutar
+vim.keymap.set("n", "<leader>ex", function()
+	local file_name = vim.api.nvim_buf_get_name(0)
+	local file_type = vim.bo.filetype
+
+	if file_type == "lua" then
+		vim.cmd(":terminal lua " .. file_name)
+	elseif file_type == "c" then
+		vim.cmd(":terminal gcc " .. file_name .. "; ./a.out")
+	elseif file_type == "python" then
+		vim.cmd(":terminal python " .. file_name)
+	end
+end)
+
 -- Abre donde se dejó la ultima vez
 local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
 vim.api.nvim_clear_autocmds({ group = lastplace })
 vim.api.nvim_create_autocmd("BufReadPost", {
-    group = lastplace,
-    pattern = { "*" },
-    desc = "remember last cursor place",
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+	group = lastplace,
+	pattern = { "*" },
+	desc = "remember last cursor place",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
