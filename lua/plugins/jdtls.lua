80374cf73efe8431b106d7fb9e8177a998fe3161
Leonardo Blautzik <leoblautzik@gmail.com>
2024-09-11 23:49:54 -0300

    neovim leoblau al 12-09-24

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -1,10 +1,11 @@
 return {
-    -- "mfussenegger/nvim-jdtls",
-    -- config = function()
-    --     local config = {
-    --         cmd = { "/home/leoblau/.local/share/nvim/mason/bin/jdtls" },
-    --         root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-    --     }
-    --     require("jdtls").start_or_attach(config)
-    -- end,
+	"mfussenegger/nvim-jdtls",
+	ft = "java", -- Enable only on .java file extensions
+	-- config = function()
+	--     local config = {
+	--         cmd = { "/home/leoblau/.local/share/nvim/mason/bin/jdtls" },
+	--         root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	--     }
+	--     require("jdtls").start_or_attach(config)
+	-- end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,89 +1,95 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "gopls", "pyright" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            -- local on_attach = require("cmp_nvim_lsp").default_on_attach()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"gopls",
+					"pyright",
+					"jdtls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- local on_attach = require("cmp_nvim_lsp").default_on_attach()
+			local lspconfig = require("lspconfig")
 
-            -- lua
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			-- lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            -- c
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
+			-- c
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-            -- go
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-                cmd = { "gopls" },
-                filetypes = { "go", "gomod", "gowork", "gotmpl" },
-                --root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            unusedparams = true,
-                        },
-                    },
-                },
-            })
+			-- go
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				--root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
 
-            -- python
-            lspconfig.pyright.setup({
-                -- on_attach = on_attach,
-                capabilities = capabilities,
-                filetypes = { "python" },
-                settings = {
-                    pyright = {
-                        disableLanguageServices = false,
-                        disableOrganizeImports = false,
-                    },
-                },
-                analysis = {
-                    autoImportCompletions = true,
-                    typeCheckingMode = "off",
-                    autoSearchPaths = true,
-                    useLibraryCodeForTypes = true,
-                    diagnosticMode = "workspace", -- "openFilesOnly",
-                },
-            })
+			-- python
+			lspconfig.pyright.setup({
+				-- on_attach = on_attach,
+				capabilities = capabilities,
+				filetypes = { "python" },
+				settings = {
+					pyright = {
+						disableLanguageServices = false,
+						disableOrganizeImports = false,
+					},
+				},
+				analysis = {
+					autoImportCompletions = true,
+					typeCheckingMode = "off",
+					autoSearchPaths = true,
+					useLibraryCodeForTypes = true,
+					diagnosticMode = "workspace", -- "openFilesOnly",
+				},
+			})
 
-            -- java
-            lspconfig.jdtls.setup({
-                on_attach = function(client, bufnr)
-                    client.server_capabilities.signatureHelpProvider = false
-                    -- on_attach(client, bufnr)
-                end,
-                capabilities = capabilities,
-            })
+			-- java
+			lspconfig.jdtls.setup({
+				on_attach = function(client, bufnr)
+					client.server_capabilities.signatureHelpProvider = false
+					-- on_attach(client, bufnr)
+				end,
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set({ "n", "v" }, "<leader>rn", vim.lsp.buf.rename, {})
-            vim.keymap.set({ "n", "v" }, "<leader>lf", vim.diagnostic.open_float, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<leader>lf", vim.diagnostic.open_float, {})
+		end,
+	},
 }
